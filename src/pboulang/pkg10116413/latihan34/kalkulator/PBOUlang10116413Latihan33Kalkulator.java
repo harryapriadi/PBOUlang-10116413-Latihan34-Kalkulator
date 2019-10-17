5fb5518f980fd899651b27540e2bfc60b61eca40
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-17 14:07:22 +0700

    PBOUlang-10116413-Latihan34-Kalkulator

---
+++ src/pboulang/pkg10116413/latihan34/kalkulator/PBOUlang10116413Latihan33Kalkulator.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan34.kalkulator;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+/**
+ *
+ * @author Lenovo
+ */
+public class PBOUlang10116413Latihan33Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     Scanner scn = new Scanner(System.in);
+        Kalkulator kal = new Kalkulator();
+
+        System.out.println("====Aplikasi Kalkulator Bilangan====");
+        System.out.print("Masukkan Angka ke-1 : ");
+        kal.value1 = scn.nextDouble();
+        System.out.print("Masukkan Angka ke-2 : ");
+        kal.value2 = scn.nextDouble();
+
+        //format dua digit di belakang koma
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        System.out.println("\nHasil Penjumlahan : "+kal.tambahBilangan());
+        System.out.println("Hasil Pengurangan : "+kal.kurangBiangan());
+        System.out.println("Hasil Perkalian : "+kal.kaliBilangan());
+        System.out.println("Hasil Pembagian : "+df.format(kal.bagiBilangan()));
+        System.out.println("Hasil Sisa : "+kal.sisaBilangan());
+
+    }
+    
+}
